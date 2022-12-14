c6af5f495bd9a23747041a5e61227dc681559696
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2022-12-14 22:17:49 +0100

    Update for other freiburg map

--- combine_images_freiburg.py
+++ combine_images_freiburg.py
@@ -1,13 +1,13 @@
 import cv2
 
 
-foler_path = "freiburg2/"
+foler_path = "freiburg3/"
 image = None
 
-for first_number in range(0, 62):
+for first_number in range(0, 21):
     vertical_image = None
-    for sec_number in range(0, 53):
-        temp_image = "6-" + str(first_number) +"-"+ str(sec_number) + ".jpg"
+    for sec_number in range(0, 35):
+        temp_image = "6-" + str(first_number) + "-" + str(sec_number) + ".jpg"
 
         try:
             new_tile = cv2.imread(foler_path + temp_image)
@@ -22,5 +22,5 @@ for first_number in range(0, 62):
         image = vertical_image
     else:
         image = cv2.hconcat([image, vertical_image])
-        
-    cv2.imwrite('freiburg_map.png', image)
+
+    cv2.imwrite('freiburg3_map.png', image)
--- dowload_images_freiburg.py
+++ dowload_images_freiburg.py
@@ -1,21 +1,22 @@
 import urllib.request
 import os
 
-base_url = "https://www.ub.uni-freiburg.de/fileadmin/ub/images/digitalisierung/stadtplaene/00019101/"
-base_folder = "freiburg2/"
+base_url = "https://www.ub.uni-freiburg.de/fileadmin/ub/images/digitalisierung/stadtplaene/0001907/"
+# base_url = "https://www.ub.uni-freiburg.de/fileadmin/ub/images/digitalisierung/stadtplaene/00019101/"
+base_folder = "freiburg3/"
 
-for folder in range(4,18):
+for folder in range(0, 4):
     folder = "TileGroup" + str(folder) + "/"
 
     os.makedirs(base_folder + folder, exist_ok=True)
 
-    for first_number in range(0, 62):
-        for second_number in range(0, 53):
-            temp_image = "6-" + str(first_number) + "-" + str(second_number) + ".jpg"
+    for first_number in range(0, 21):
+        for second_number in range(0, 35):
+            temp_image = "6-" + str(first_number) + \
+                "-" + str(second_number) + ".jpg"
 
             try:
                 urllib.request.urlretrieve(
                     base_url + folder + temp_image, base_folder + folder + temp_image)
             except:
                 print(folder + temp_image)
-
